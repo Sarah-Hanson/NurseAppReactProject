048136d1a99b9d6d2bbb76b72f3f3e89df76330a
Sarah <Sarah@mygastank.ca>
2021-05-03 16:11:03 -0600

    changed room page to use the common box instead of its custom box

--- app/src/Pages/RoomsPage.tsx
+++ app/src/Pages/RoomsPage.tsx
@@ -1,6 +1,14 @@
 import {useStateValue} from '../StateProvider';
 import {PageWrapper} from '../Common/PageWrapper';
-import {Card, colors, Row, Spacer, TextBig, TextSmall} from '../Common/common';
+import {
+  Box,
+  Card,
+  colors,
+  Row,
+  Spacer,
+  TextBig,
+  TextSmall,
+} from '../Common/common';
 import React from 'react';
 import {IBed, Room} from '../floorPlan';
 import {ScrollView, TextInput, TouchableOpacity, View} from 'react-native';
@@ -41,7 +49,15 @@ const BedLine = ({roomId, bed}: {roomId: string; bed: IBed}) => {
 
   return (
     <Row key={bed.id}>
-      <Box roomId={roomId} bed={bed} />
+      <Box
+        isSet={bed.active}
+        onPress={() =>
+          dispatch({
+            type: Actions.toggleBed,
+            payload: {roomId, bedId: bed.id},
+          })
+        }
+      />
       <TextSmall text={bed.name} style={{width: '20%'}} />
       <TextSmall text={'Acuity:'} style={{width: '20%'}} />
       <TouchableOpacity
@@ -65,29 +81,5 @@ const BedLine = ({roomId, bed}: {roomId: string; bed: IBed}) => {
       </TouchableOpacity>
       <Spacer />
     </Row>
-  );
-};
-
-const Box = ({roomId, bed}: {roomId: string; bed: IBed}) => {
-  // @ts-ignore
-  const [state, dispatch] = useStateValue();
-
-  return (
-    <TouchableOpacity
-      style={{
-        height: 15,
-        width: 15,
-        borderColor: colors.white,
-        borderWidth: 2,
-        borderRadius: 1,
-        backgroundColor: bed.active ? colors.white : colors.black,
-      }}
-      onPress={() =>
-        dispatch({
-          type: Actions.toggleBed,
-          payload: {roomId, bedId: bed.id},
-        })
-      }
-    />
   );
 };
